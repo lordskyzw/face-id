aacbf82b31a71ca8c93626142e58a122254cdbf4
lordskyzw <lilskyforever@gmail.com>
2024-02-28 01:04:27 +0200

    focus on adding people

--- app.py
+++ app.py
@@ -20,6 +20,72 @@ def test():
     }
 
 
+@app.route('/add_person', methods=['POST'])
+def add_person():
+# scenarios to consider
+# 1 photo
+# multiple photos
+
+# 1 photo
+# if its one photo, it could be a photo of a person or a photo with multiple people
+    
+    
+    
+    
+#this function receives images and adds them to a general folder first 
+#then  it extracts faces from the images and attempts to recognize the faces before and save them in a folder(s) with person(s) name(S)
+#one picture can contain multiple faces and the function will create a folder for each person
+#if the persons name already exists, it adds the new faces to the existing folder
+#if the persons name does not exist, it creates a new folder with the persons name
+#the function to extract faces is DeepFace.extract_faces()
+
+    req = request.get_json()
+    try:
+        base_64_image = req['image']
+        image_data = base64.b64decode(base_64_image)
+        nparr = np.frombuffer(image_data, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        face_detected = DeepFace.extract_faces(img, enforce_detection=False, detector_backend='yolov8')
+        unprocessed = "unprocessed"
+        
+        if len(face_detected)>0:
+            # Save the image to the dataset folder
+            image_path = f"{db_path}/unprocessed/"
+            if not os.path.exists(image_path):
+                os.makedirs(image_path)
+            cv2.imwrite(f"{image_path}/{unprocessed}_{len(os.listdir(image_path))}.jpg", img)
+            for face in face_detected:
+                # attempt to recognize the face
+                recognition_result = DeepFace.find(img_path=face, db_path=db_path, enforce_detection=False)
+                faces_df = recognition_result[0]
+                tup = recognition_result[0].shape
+                
+                if tup[0]>1:
+                    identity = faces_df.iloc[0,0]
+                    person_name = identity.split('/')[-2]
+                    # Save the face to the person's folder
+                    image_path = f"{db_path}/{person_name}/"
+                    if not os.path.exists(image_path):
+                        os.makedirs(image_path)
+                    cv2.imwrite(f"{image_path}/{person_name}_{len(os.listdir(image_path))}.jpg", img)
+                    
+            return {
+                'statusCode': 200,
+                'body': json.dumps('Person added successfully')
+            }
+        else:
+            return {
+                'statusCode': 200,
+                'body': json.dumps('No face detected in the image')
+            }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f'Error processing the image: {str(e)}')
+        }
+
+
+
 @app.route('/facial_recognition', methods=['POST'])
 def facial_recognition():
     req = request.get_json()
